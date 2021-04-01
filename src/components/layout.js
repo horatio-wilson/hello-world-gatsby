36c5ab3ead91da2af5a5a334e95dd59b0c9e07e5
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2021-04-01 23:30:36 +0200

    Added a page which displays the application file system

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
@@ -7,11 +9,22 @@ const ListLink = props => (
 )
 
 export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>My Gatsby Site</h3>
+          <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
           <ListLink to="/">Home</ListLink>
